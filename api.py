c587ed25e7adfe5cfcac4491da4b2770ac11deb1
xando <sebastian.pawlus@gmail.com>
2014-01-01 18:27:49 +0100

    Basci api validation

--- api.py
+++ api.py
@@ -1,19 +1,49 @@
-from flask import Flask, request, abort
-from flask.ext.restful import Resource, Api
+from flask import Flask
+from flask.ext.restful import Resource, Api, reqparse, abort
 
-from game import worlds
-
+from game import worlds, Player
 
 app = Flask(__name__)
 api = Api(app)
 
+parser = reqparse.RequestParser()
+parser.add_argument('script', type=str)
+
+
 class World(Resource):
 
-    def get(self, hash):
+    def get_world(self, world_id):
         try:
-            return worlds[hash]().map
+            return worlds[world_id]
         except KeyError:
             abort(404)
+
+    def get(self, hash):
+        return self.get_world(hash).map
+
+    def post(self, hash):
+
+        data = parser.parse_args()
+        world = self.get_world(hash)
+        player = Player(world)
+        script = data['script']
+
+        try:
+            # Wohooh, so naive
+            # ToDo: some kind of sandbox
+            exec(script, {}, {
+                'world': world,
+                'guy': player,
+            })
+        except SyntaxError as e:
+            return {
+                "offset": e.offset,
+                "lineno": e.lineno,
+                "text": e.text,
+                "message": e.msg
+            }, 204
+
+        return {}, 200
 
 
 api.add_resource(World, '/world/<string:hash>')
--- tests/test_api.py
+++ tests/test_api.py
@@ -5,6 +5,7 @@ import json
 
 from game import worlds
 
+
 @pytest.fixture
 def app():
     from api import app
@@ -18,3 +19,12 @@ def test_get_world(app):
     assert response.status_code == 200
     assert worlds[world_hash].map == json.loads(response.data.decode('utf-8'))
 
+
+def test_post_script(app):
+    response = app.post('/world/1', data={"script": 'print("test")'})
+    assert response.status_code == 200
+
+
+def test_post_script_error(app):
+    response = app.post('/world/1', data={"script": 'print("test)'})
+    assert response.status_code == 204
